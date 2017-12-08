a8fc6bda31e43a26c9df201962d434a0b236cb88
Phoebe Lau <phoebelau7@gmail.com>
2017-12-08 15:15:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,18 +7,6 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def add_song_by_name(song)
-    song = Song.new
-    @songs << song
-    @@song_count += 1
-    song.artist = self
-  end
 
   def self.song_count
     @@song_count
