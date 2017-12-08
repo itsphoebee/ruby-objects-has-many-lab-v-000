555836be02818c2eee41666e340e4e8bc8e3bbb3
Phoebe Lau <phoebelau7@gmail.com>
2017-12-08 15:03:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
   @@song_count
 
   def initialize(name)
@@ -10,12 +10,13 @@ class Artist
   def add_song(song)
     song = Song.new
     @songs << song
-    song.artist = self
   end
 
   def add_song_by_name(song_name)
-
-
+    song_name = Song.new
+    
+  end
+  
   def self.song_count
     @@song_count
   end
