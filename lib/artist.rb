0e42b91a103858ceddf671442e19c4d793aad108
Phoebe Lau <phoebelau7@gmail.com>
2017-12-08 16:19:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,12 +14,12 @@ class Artist
   end
 
   def songs       #why did i need a method called songs if instance variable @songs was
-    @songs        #created in initialize?
+    @songs        #created in initialize? / bc need the method to access it ?
   end
 
   def add_song_by_name(song)
-    song =Song.new(song)
-    add_song(song)
+    song =Song.new(song)          #create a new song using the arugment string provided
+    add_song(song)                #calls the add_song method to operate on the new song
   end
 
   def self.song_count
